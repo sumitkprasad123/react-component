ec31c9f6ee76badf851558308a0286c352f15882
sumit <sumitkprasad123@gmail.com>
2023-11-29 22:28:23 +0530

    navbar

---
+++ rec-comp/src/components/Navbar.jsx/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import "./navbar.css"
+
+const Navbar = () => {
+const [open,setOpen] = useState(false)
+  return (
+    <div className="navbar">
+        <div className="container">
+            <div className="logo">Sumit</div>
+            <div className="menu">
+                <span className="menu_title">Home</span>
+                <span className="menu_title">Profile</span>
+                <div className="option" onClick={()=>setOpen(!open)}>
+                    <span className="menu_title">Catogery</span>
+                    <div className={open?"option_menu":"option_none"}>
+                        <span className='option_menu_item'>Boy Item</span>
+                        <span className='option_menu_item'>Girl Item</span>
+                        <span className='option_menu_item'>Men Item</span>
+                        <span className='option_menu_item'>Women Item</span>
+                    </div>
+                    
+                </div>
+                <span className="menu_title">Sign In</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
